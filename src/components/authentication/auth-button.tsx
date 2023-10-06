267eb0efe4ead30c7b245942c0c43ab38d3ecaff
Mauro Eijsenring <mauro@eijsenring.com>
2023-10-06 11:10:05 +0200

    update auth component to use signing function

--- src/components/authentication/auth-button.tsx
+++ src/components/authentication/auth-button.tsx
@@ -1,73 +1,119 @@
 /* eslint-disable @typescript-eslint/unbound-method */
-import { useEffect } from "react";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { useWallet } from "@demox-labs/aleo-wallet-adapter-react";
+
+import { useEffect, useRef, useState } from "react";
+import { useSession } from "next-auth/react";
 import {
-  DecryptPermission,
-  type WalletAdapterNetwork,
-} from "@demox-labs/aleo-wallet-adapter-base";
-import { LeoWalletName } from "@demox-labs/aleo-wallet-adapter-leo";
+  LeoWalletName,
+  type LeoWalletAdapter,
+} from "@demox-labs/aleo-wallet-adapter-leo";
 import { Button } from "@mui/material";
 import { isValidSession } from "@/utils/session";
 import { text } from "@/assets/text";
-import { env } from "@/env.mjs";
+import { useWallet } from "@demox-labs/aleo-wallet-adapter-react";
+import { signIn, signOut } from "next-auth/react";
+import {
+  DecryptPermission,
+  WalletAdapterNetwork,
+} from "@demox-labs/aleo-wallet-adapter-base";
+
+const AUTH_SIGN_MESSAGE = "Sign in to Bot Busters";
 
 export const AuthButton = () => {
   const { data: sessionData, status } = useSession();
-
   const {
     publicKey,
-    connect,
-    connected: walletConnected,
+    wallet,
     select,
+    connect,
+    connected,
+    connecting,
     disconnect,
   } = useWallet();
 
-  useEffect(() => {
-    select(LeoWalletName);
-  }, [select]);
+  const [signature, setSignature] = useState<string>();
+  const [walletStatus, setWalletStatus] = useState<
+    "connecting" | "checking-signature" | "success" | "error"
+  >();
+  const isCheckingSign = useRef(false);
+  const authTriggered = useRef(false);
 
   useEffect(() => {
-    const authenticate = async () => {
-      if (!walletConnected || !publicKey || status === "authenticated") return;
+    const connectWallet = async () => {
+      if (
+        connecting ||
+        !wallet ||
+        !publicKey ||
+        !authTriggered.current ||
+        isCheckingSign.current
+      )
+        return;
+      isCheckingSign.current = true;
+
       try {
-        await signIn("credentials", { publicKey });
+        const adapter = wallet.adapter as LeoWalletAdapter;
+
+        let newSignature = signature;
+        if (!newSignature) {
+          const bytes = new TextEncoder().encode(AUTH_SIGN_MESSAGE);
+          const signatureBytes = await adapter.signMessage(bytes);
+          newSignature = new TextDecoder().decode(signatureBytes);
+
+          setSignature(newSignature);
+        }
+        await signIn("credentials", {
+          publicKey,
+          playerSign: newSignature,
+          message: AUTH_SIGN_MESSAGE,
+        });
+        isCheckingSign.current = false;
+        authTriggered.current = false;
       } catch (error) {
         console.error(error);
+        setWalletStatus("error");
+      } finally {
+        setSignature(undefined);
       }
     };
-    void authenticate();
-  }, [publicKey, status, walletConnected]);
+    void connectWallet();
+  }, [wallet, publicKey, connecting, signature]);
 
-  const handleSignIn = async () => {
+  const authenticatePlayer = async () => {
     try {
-      await connect(
-        DecryptPermission.UponRequest,
-        env.NEXT_PUBLIC_ALEO_NETWORK as WalletAdapterNetwork
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleSignOut = async () => {
-    try {
-      await disconnect();
-      await signOut();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <Button
-      onClick={
-        isValidSession(sessionData)
-          ? () => void handleSignOut()
-          : () => void handleSignIn()
+      setWalletStatus("connecting");
+      select(LeoWalletName);
+      if (!connected) {
+        await connect(
+          DecryptPermission.UponRequest,
+          WalletAdapterNetwork.Testnet
+        );
       }
-    >
-      {isValidSession(sessionData) ? text.auth.signOut : text.auth.signIn}
-    </Button>
+      setWalletStatus("checking-signature");
+      authTriggered.current = true;
+    } catch (error) {
+      console.warn(error);
+      setWalletStatus("error");
+    }
+  };
+  const logout = async () => {
+    await signOut();
+    await disconnect();
+  };
+  return (
+    <>
+      <p>
+        <a>Wallet status: {walletStatus}</a>
+        <br />
+        <a>Session status: {status}</a>
+      </p>
+      <Button
+        onClick={
+          isValidSession(sessionData)
+            ? () => void logout()
+            : () => void authenticatePlayer()
+        }
+      >
+        {isValidSession(sessionData) ? text.auth.signOut : text.auth.signIn}
+      </Button>
+    </>
   );
 };
