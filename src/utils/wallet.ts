49de18c54d364a35714940600910d7c14306b8b7
Mauro Eijsenring <mauro@eijsenring.com>
2023-10-10 12:09:03 +0200

    Moved util function to new file called wallet

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,14 +1,14 @@
 import { Address, Signature } from "@aleohq/sdk";
 
 export const verifySignature = (
-  pubAddress: string,
+  publicKey: string,
   message: string,
-  playerSign: string
+  signedMessage: string
 ): boolean => {
   const messageBuffer = new TextEncoder().encode(message);
 
-  const signature = Signature.from_string(playerSign);
-  const address = Address.from_string(pubAddress);
+  const signature = Signature.from_string(signedMessage);
+  const address = Address.from_string(publicKey);
 
   const isVerified = signature.verify(address, messageBuffer);
 
