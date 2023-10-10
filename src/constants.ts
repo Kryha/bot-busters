0f7e7912df92b1cfbf6c643a90ee941bc77011b1
Mauro Eijsenring <mauro@eijsenring.com>
2023-10-10 12:11:17 +0200

    add app_name constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const PUBLIC_KEY_LENGTH = 63;
 export const PLACEHOLDER_TEXT = "botBusters";
 export const AUTH_SIGN_MESSAGE = "Sign in to Bot Busters";
+export const APP_NAME = "Bot Busters";
