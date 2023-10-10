cfff4e0fda5a7d943b73a33fd5cce5056cec603b
Marius Sili <sili.marius.me@gmail.com>
2023-10-10 12:03:16 +0200

    Configured container deployment and Github workflow (#24)
    ## Description
    
    This PR introduces configurations for container build, deployment and
    Github workflow.
    
    ## Related issues
    
    #10 
    
    ## Changes made
    - Containerised the application with a Dockerfile
    - Configured skaffold for local and staging build and deployment
    - Defined kustomize files for local and staging deployment
    - Defined workflow to deploy on staging environment
    - Not using LFS to track cache because of issues when building on Github
    - `instrumentation.ts` now also pushes the db schemas

---
+++ deploy_ingress_controller.sh
@@ -0,0 +1,3 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.2.0/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml
+kubectl apply -f deployment/templates/cert-manager/
