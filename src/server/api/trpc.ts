edbf2c8ce69c038dcf62adac96a451e4d8e8ee1d
Marius Sili <sili.marius.me@gmail.com>
2023-10-12 17:02:58 +0200

    Logging session

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -112,13 +112,14 @@ export const publicProcedure = t.procedure;
 
 /** Reusable middleware that enforces users are logged in before running the procedure. */
 const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
-  if (!ctx.session?.publicKey) {
+  console.log("SESSION IN AUTH:", ctx.session);
+  if (!ctx.session?.user) {
     throw new TRPCError({ code: "UNAUTHORIZED" });
   }
   return next({
     ctx: {
       // infers the `session` as non-nullable
-      session: { ...ctx.session },
+      session: { ...ctx.session, user: ctx.session.user },
     },
   });
 });
@@ -127,7 +128,7 @@ const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
  * Protected (authenticated) procedure
  *
  * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
- * the session is valid and guarantees `ctx.session.publicKey` is not null.
+ * the session is valid and guarantees `ctx.session.user` is not null.
  *
  * @see https://trpc.io/docs/procedures
  */
