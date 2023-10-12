151954eea5479eb2256c53bf95cecd7f1033b89c
Marius Sili <sili.marius.me@gmail.com>
2023-10-12 16:48:34 +0200

    Checking publicKey instead of user in session

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -112,13 +112,13 @@ export const publicProcedure = t.procedure;
 
 /** Reusable middleware that enforces users are logged in before running the procedure. */
 const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
-  if (!ctx.session?.user) {
+  if (!ctx.session?.publicKey) {
     throw new TRPCError({ code: "UNAUTHORIZED" });
   }
   return next({
     ctx: {
       // infers the `session` as non-nullable
-      session: { ...ctx.session, user: ctx.session.user },
+      session: { ...ctx.session },
     },
   });
 });
@@ -127,7 +127,7 @@ const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
  * Protected (authenticated) procedure
  *
  * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
- * the session is valid and guarantees `ctx.session.user` is not null.
+ * the session is valid and guarantees `ctx.session.publicKey` is not null.
  *
  * @see https://trpc.io/docs/procedures
  */
