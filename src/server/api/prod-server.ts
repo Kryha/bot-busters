b6fac96c7695fcb8b6f0c13c7a9382473399e4cf
Marius Sili <sili.marius.me@gmail.com>
2023-10-16 11:46:12 +0200

    Writing head

--- src/server/api/prod-server.ts
+++ src/server/api/prod-server.ts
@@ -12,6 +12,17 @@ const app = next({ dev: env.NODE_ENV !== "production" });
 const handle = app.getRequestHandler();
 
 const server = http.createServer((req, res) => {
+  const proto = req.headers["x-forwarded-proto"];
+  if (proto && proto === "http") {
+    // redirect to ssl
+    res.writeHead(303, {
+      location:
+        `https://` + req.headers.host + ((req.headers.url as string) ?? ""),
+    });
+    res.end();
+    return;
+  }
+
   const parsedUrl = parse(req.url!, true);
   void handle(req, res, parsedUrl);
 });
@@ -39,6 +50,7 @@ wss.on("connection", (ws) => {
 process.on("SIGTERM", () => {
   console.log("SIGTERM");
   handler.broadcastReconnectNotification();
+  wss.close();
 });
 
 void app.prepare().then(() => {
