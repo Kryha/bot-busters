16bd8f37c3a3c397dcc8c4e936429fc935c4d7fc
Frank Benjamin Arias <77683125+frankbenji@users.noreply.github.com>
2023-10-19 13:41:06 +0200

    added grey mui theme

--- src/features/mui/palette.ts
+++ src/features/mui/palette.ts
@@ -1,7 +1,20 @@
 import { type Palette, type Theme } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 export const palette = (theme: Theme): Palette => {
   return {
     ...theme.palette,
+    primary: {
+      light: grey.A700,
+      main: grey[900],
+      dark: grey[800],
+      contrastText: grey[50],
+    },
+    secondary: {
+      light: grey.A200,
+      main: grey[300],
+      dark: grey[400],
+      contrastText: grey[900],
+    },
   };
 };
