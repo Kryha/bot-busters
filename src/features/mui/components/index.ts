7b4a7258d4abf519ebf242dca0b2e9b1ecaac272
Frank Benjamin Arias <77683125+frankbenji@users.noreply.github.com>
2023-10-20 13:20:19 +0200

    removed scaling styles

--- src/features/mui/components/index.ts
+++ src/features/mui/components/index.ts
@@ -2,16 +2,10 @@ import { type Components, type Theme } from "@mui/material";
 
 import { MuiButton } from "./mui-button";
 import { MuiCard } from "./mui-card";
-import { MuiIconButton } from "./mui-icon-button";
-import { MuiSvgIcon } from "./mui-svg-icon";
-import { MuiAvatar } from "./mui-avatar";
 
 export const components = (theme: Theme): Components => {
   return {
     MuiButton: MuiButton(theme),
     MuiCard: MuiCard(theme),
-    MuiIconButton: MuiIconButton(),
-    MuiSvgIcon: MuiSvgIcon(theme),
-    MuiAvatar: MuiAvatar(theme),
   };
 };
