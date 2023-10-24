2f4145f2a79a9579f1342adc5e48f8e803fcaba2
Mauro Eijsenring <mauro@eijsenring.com>
2023-10-24 16:03:15 +0200

    Removed random string function

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -14,14 +14,3 @@ export const verifySignature = (
 
   return isVerified;
 };
-
-//TODO: remove this when we have random username generation
-export const generateRandomString = (length: number) => {
-  const letters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let randomString = "";
-  for (let i = 0; i < length; i++) {
-    randomString += letters[Math.floor(Math.random() * letters.length)];
-  }
-  return randomString;
-};
