c3c14ad9609876e323ecb7b82b9304d17d6877cc
Mauro Eijsenring <mauro@eijsenring.com>
2023-10-25 13:14:00 +0200

    Utility function for session status

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -2,3 +2,13 @@ import { type Session } from "next-auth";
 
 export const isValidSession = (session: Session | null) =>
   Boolean(session?.id && session?.id !== "null");
+
+export const isAnonymousSession = (session: Session | null) => {
+  const validSession = isValidSession(session);
+  if (validSession && !session?.address && !session?.username) return true;
+};
+
+export const isVerifiedSession = (session: Session | null) => {
+  if (!isValidSession(session)) return false;
+  if (session?.address && session?.username) return true;
+};
