eeae3c894a82e5921db15f5fd4ee9da1b719cd9f
Mauro Eijsenring <mauro@eijsenring.com>
2023-11-07 16:26:12 +0100

    Hotfix staging (#81)
    This is a fix for the undefined error when running the app

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,16 +2,12 @@ import { env } from "@/env.cjs";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-import * as schema from "./schema/";
-import { pgTableCreator } from "drizzle-orm/pg-core";
-
-export const dbSchema = schema;
-
 const queryClient = postgres(env.DATABASE_URL);
+
+import * as schema from "./schema/";
 export const db = drizzle(queryClient, { schema });
 
-export const bbPgTable = pgTableCreator((name) => `bot_busters_${name}`);
-
+export const dbSchema = schema;
 export const closeDbConnection = async () => {
   await queryClient.end();
 };
