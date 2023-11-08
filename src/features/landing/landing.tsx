8612940ed3a74c82adb3d1e88b20bc050dfe0571
Mauro Eijsenring <mauro@eijsenring.com>
2023-11-08 13:00:16 +0100

    added user route to trpc router

--- src/features/landing/landing.tsx
+++ src/features/landing/landing.tsx
@@ -5,16 +5,21 @@ import { isValidSession, isVerifiedSession } from "@/utils/session";
 import { fakeUsername } from "@/constants";
 import { StartGame, UserStats } from "./components";
 import { LeaderBoard } from "@/features/leaderboard";
+import { api } from "@/utils/api";
 
 export const Landing: FC = () => {
   const { data: sessionData } = useSession();
   const isAuthenticated = isValidSession(sessionData);
   const isWalledConnected = isVerifiedSession(sessionData);
   const isGamePlayed = true;
+  const { data } = api.user.getUserById.useQuery();
+  const getInfo = () => {
+    return `Is Authenticated with this ID: ${sessionData?.id} with score ${data?.score} `;
+  };
 
   return (
     <>
-      {isAuthenticated && <a>Is Authenticated</a>}
+      {isAuthenticated && <a>{getInfo()}</a>}
       <UserStats
         isWalletConnected={isWalledConnected}
         isGamePlayed={isGamePlayed}
