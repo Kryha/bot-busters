ffb1621a3c9ed841b888463a99a6fc9bd470711d
Frank Benjamin Arias <77683125+frankbenji@users.noreply.github.com>
2023-11-10 13:29:27 +0100

    removed landing feature

--- src/features/landing/components/points-display/styles.ts
+++
@@ -1,8 +0,0 @@
-import { type SxProps } from "@mui/material";
-
-export const styles = {
-  pointsContainer: (isCentered?: boolean) => {
-    return { alignItems: isCentered ? "center" : "flex-start" };
-  },
-  pointsWrapper: { flexDirection: "row", gap: 5 } satisfies SxProps,
-};
