12a372454a88dfe369261dfb57fd4a7809da9dbb
Frank Benjamin Arias <77683125+frankbenji@users.noreply.github.com>
2023-11-10 09:52:04 +0100

    Lobby page (#87)
    - Added lobby page
    - Added splash screen

---
+++ src/layouts/lobby-layout/styles.ts
@@ -0,0 +1,14 @@
+import { type SxProps } from "@mui/material";
+
+export const styles = {
+  container: {
+    minHeight: "50vh",
+    overflow: "scroll",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    gap: "80px",
+    maxWidth: "1800px !important",
+  } satisfies SxProps,
+};
