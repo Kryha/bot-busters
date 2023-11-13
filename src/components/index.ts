2be5260d67b8789f827bc3c52eacd1ebd64ce854
Frank Benjamin Arias <77683125+frankbenji@users.noreply.github.com>
2023-11-13 11:50:40 +0100

    Removed start game component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from "./authentication";
 export * from "./error-view";
 export * from "./tables";
-export * from "./start-game";
 export * from "./user-menu";
 export * from "./user-stats";
 export * from "./points-display";
