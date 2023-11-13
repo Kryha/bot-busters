f92047da9459ccb21fc8997188d8b4a60932fcf1
Frank Benjamin Arias <77683125+frankbenji@users.noreply.github.com>
2023-11-13 16:11:44 +0100

    removed user-stats

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,5 @@ export * from "./authentication";
 export * from "./error-view";
 export * from "./tables";
 export * from "./user-menu";
-export * from "./user-stats";
 export * from "./points-display";
 export * from "./top-ranked";
