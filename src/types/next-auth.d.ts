ca866793090700a786c6d4bcc740e7e86fc6e782
Mauro Eijsenring <mauro@eijsenring.com>
2023-11-14 16:28:09 +0100

    Add NextAuth types for user and session

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,26 @@
+import { JWT } from "next-auth/jwt";
+import NextAuth, { type DefaultSession, type ISODateString } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      address?: string;
+      username?: string;
+    };
+    expires: ISODateString;
+  }
+  interface User {
+    id: string;
+    username?: string;
+    address?: string;
+  }
+}
+declare module "next-auth/jwt" {
+  interface JWT {
+    idToken?: string;
+    userID: string;
+    address?: string;
+    username?: string;
+  }
+}
