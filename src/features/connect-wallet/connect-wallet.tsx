52c68158db17209e51356bbff869b70ece7e87c9
Mauro Eijsenring <mauro@eijsenring.com>
2023-11-14 16:44:45 +0100

    Add verification check and redirect to home page if user is verified.

--- src/features/connect-wallet/connect-wallet.tsx
+++ src/features/connect-wallet/connect-wallet.tsx
@@ -2,7 +2,9 @@ import { type FC } from "react";
 
 import { UsernameSelect, ConnectPlaceholder } from "./components";
 import { useSession } from "next-auth/react";
-import { isUnverifiedSession } from "@/utils/session";
+import { isUnverifiedSession, isVerifiedSession } from "@/utils/session";
+import router from "next/router";
+import { pages } from "@/utils/router";
 
 export const ConnectWallet: FC = () => {
   const { data: sessionData } = useSession();
@@ -10,6 +12,6 @@ export const ConnectWallet: FC = () => {
   // TODO: add check if the user already has a username & id
 
   if (isUnverifiedSession(sessionData)) return <UsernameSelect />;
-
+  if (isVerifiedSession(sessionData)) void router.push(pages.home);
   return <ConnectPlaceholder />;
 };
