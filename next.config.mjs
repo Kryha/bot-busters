cb0a3e141a944b81f5fd6314adeb233f5c54ba54
Marius Sili <sili.marius.me@gmail.com>
2023-11-24 09:44:37 +0100

    js extension next config

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,15 @@ const config = {
   experimental: {
     instrumentationHook: true,
   },
+
+  webpack: (webpackConfig) => {
+    webpackConfig.resolve.extensionAlias = {
+      ".js": [".ts", ".tsx", ".js", ".jsx"],
+      ".mjs": [".mts", ".mjs"],
+      ".cjs": [".cts", ".cjs"],
+    };
+    return webpackConfig;
+  },
 };
 
 export default config;
