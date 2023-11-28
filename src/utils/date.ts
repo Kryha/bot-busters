3185b8a62dd503d7bfc812b9c647890193763338
Privilege Mendes <privilege@kryha.io>
2023-11-28 22:52:19 +0100

    added timestamp util function for chat messages

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -85,30 +85,3 @@ export const getTimeStamp = (time: number) => {
   const formattedMinutes = minutes.toString().padStart(2, "0");
   return `${formattedHours}:${formattedMinutes}`;
 };
-
-export const getTimeStamp = (time: number) => {
-  const date = new Date(time);
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const formattedHours = hours.toString().padStart(2, "0");
-  const formattedMinutes = minutes.toString().padStart(2, "0");
-  return `${formattedHours}:${formattedMinutes}`;
-};
-
-export const getTimeStamp = (time: number) => {
-  const date = new Date(time);
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const formattedHours = hours.toString().padStart(2, "0");
-  const formattedMinutes = minutes.toString().padStart(2, "0");
-  return `${formattedHours}:${formattedMinutes}`;
-};
-
-export const getTimeStamp = (time: number) => {
-  const date = new Date(time);
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const formattedHours = hours.toString().padStart(2, "0");
-  const formattedMinutes = minutes.toString().padStart(2, "0");
-  return `${formattedHours}:${formattedMinutes}`;
-};
