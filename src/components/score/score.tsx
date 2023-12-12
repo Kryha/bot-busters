2092165ae0affb062113384199fcecc7e7ceb5dc
Mauro Eijsenring <mauro@eijsenring.com>
2023-12-12 14:54:00 +0100

    Added frontend representation of achievements gained

--- src/components/score/score.tsx
+++ src/components/score/score.tsx
@@ -3,16 +3,39 @@ import { Divider, Stack, Typography } from "@mui/material";
 
 import { styles } from "./styles.js";
 import { text } from "./text.js";
+import { MATCH_ACHIEVEMENTS } from "~/server/service/achievements.js";
 
 interface Props {
   correctGuesses: number;
   gainedScore: number;
+  achievements: string[];
 }
 
-export const Score: FC<Props> = ({ correctGuesses, gainedScore }) => {
+export const Score: FC<Props> = ({
+  correctGuesses,
+  gainedScore,
+  achievements,
+}) => {
   return (
     <Stack sx={styles.container}>
       <Divider />
+      <Stack sx={styles.score}>
+        <Typography variant="body1">Achievements:</Typography>
+        {achievements.map((achievmentID) => {
+          const achievement = MATCH_ACHIEVEMENTS.get(achievmentID);
+          if (!achievement) return;
+          return (
+            <>
+              <Typography key={achievmentID} variant="body1">
+                {achievement?.name}
+              </Typography>
+              <Typography variant="body1">
+                {text.points(achievement?.points)}
+              </Typography>
+            </>
+          );
+        })}
+      </Stack>
       <Stack sx={styles.score}>
         <Typography variant="body1">
           {text.yourScore(correctGuesses)}
