aaa9d3d5a0d373a13a030534b5fe552428eb6009
Privilege Mendes <privilege@kryha.io>
2023-12-14 11:50:04 +0100

    added icons and new text

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,17 @@
+import { ReactComponent as SelectIcon } from "./menu-button-select.svg";
+import { ReactComponent as BotBustersIcon } from "./botbusters-icon.svg";
+import { ReactComponent as UserIcon } from "./user-icon.svg";
+import { ReactComponent as SoundOffIcon } from "./sound-off.svg";
+import { ReactComponent as SoundOnIcon } from "./sound-on.svg";
+import { ReactComponent as CloseIcon } from "./close-icon.svg";
+import { ReactComponent as BotBustersLogo } from "./botbusters-logo.svg";
+
+export {
+  BotBustersLogo,
+  SelectIcon,
+  BotBustersIcon,
+  UserIcon,
+  SoundOffIcon,
+  SoundOnIcon,
+  CloseIcon,
+};
