bb8722d6d4647d7ce313513229adc44f5a3143a3
Privilege Mendes <privilege@kryha.io>
2023-12-14 12:04:23 +0100

    added fonts and updated design kit

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,12 +1,42 @@
 import { type Typography } from "@mui/material/styles/createTypography.js";
-import { Roboto } from "next/font/google";
-
+import { PT_Serif } from "next/font/google";
 import { COEFFICIENT, FONT_SIZE } from "~/constants/index.js";
+import localFont from "next/font/local";
 
-const font = Roboto({
-  weight: ["100", "300", "400", "500", "700", "900"],
+const ptSerif = PT_Serif({
+  weight: ["400"],
   style: ["normal"],
   subsets: ["latin"],
+});
+
+const disposableDigi = localFont({
+  src: [
+    {
+      path: "./fonts/dispdigibb/DisposableDigiBB_italics.otf",
+      weight: "400",
+      style: "italic",
+    },
+  ],
+});
+
+const eightBitDigi = localFont({
+  src: [
+    {
+      path: "./fonts/8bit16.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+});
+
+const appleIIPro = localFont({
+  src: [
+    {
+      path: "./fonts/AppleII.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
 });
 
 export const typography = (): Typography => {
@@ -14,7 +44,7 @@ export const typography = (): Typography => {
   const pxToRem = (px: number) => `${(px / FONT_SIZE) * coefficient}rem`;
 
   return {
-    fontFamily: font.style.fontFamily,
+    fontFamily: appleIIPro.style.fontFamily,
     fontSize: FONT_SIZE,
     fontWeightLight: 300,
     fontWeightRegular: 400,
@@ -23,76 +53,95 @@ export const typography = (): Typography => {
     htmlFontSize: 16,
     pxToRem: pxToRem,
     h1: {
-      fontFamily: font.style.fontFamily,
-      fontSize: "88px",
-      fontWeight: 400,
-      lineHeight: "96px",
+      fontFamily: disposableDigi.style.fontFamily,
+      fontSize: "clamp(3.0rem, 7.4vw + 1rem, 7rem);", //150px
+      lineHeight: "clamp(5rem, 9.4vw + 1rem, 8rem);", //
+      color: "#FF8400 !important",
+      textTransform: "uppercase" as const,
     },
     h2: {
-      fontSize: "57px",
-      lineHeight: "60px",
-      fontFamily: font.style.fontFamily,
+      fontSize: "clamp(3.0rem, 7.4vw + 1rem, 7rem);", //110px
+      lineHeight: "clamp(5rem, 9.4vw + 1rem, 8rem);",
+      fontFamily: disposableDigi.style.fontFamily,
       fontWeight: 400,
+      textTransform: "uppercase" as const,
     },
     h3: {
-      fontFamily: font.style.fontFamily,
-      fontSize: "48px",
+      fontFamily: eightBitDigi.style.fontFamily,
+      fontSize: "clamp(1.5rem, 2vw + 1rem, 3rem);",
       fontWeight: 400,
-      lineHeight: "50px",
+      lineHeight: "49.65px",
+      textTransform: "uppercase" as const,
     },
     h4: {
-      fontFamily: font.style.fontFamily,
-      fontSize: "34px",
+      fontFamily: eightBitDigi.style.fontFamily,
+      fontSize: "clamp(0.5rem, 2vw + 1rem, 2rem);",
       fontWeight: 400,
-      lineHeight: "40px",
+      lineHeight: "38.1px",
+      textTransform: "uppercase" as const,
     },
     h5: {
-      fontFamily: font.style.fontFamily,
-      fontSize: "23px",
+      fontFamily: appleIIPro.style.fontFamily,
+      fontSize: "48px",
       fontWeight: 400,
-      lineHeight: "32px",
+      lineHeight: "72.09px",
     },
     h6: {
-      fontFamily: font.style.fontFamily,
+      fontFamily: appleIIPro.style.fontFamily,
       fontSize: "24px",
       fontWeight: 400,
       lineHeight: "33.6px",
     },
     subtitle1: {
-      fontFamily: font.style.fontFamily,
+      // top ranked title
+      fontFamily: disposableDigi.style.fontFamily,
+      fontSize: "48px",
+      fontWeight: 400,
+      lineHeight: "49.65px",
+      letterSpacing: "-5%",
+      textTransform: "uppercase" as const,
     },
     subtitle2: {
-      fontFamily: font.style.fontFamily,
+      // top ranked positions
+      fontFamily: appleIIPro.style.fontFamily,
+      fontSize: pxToRem(20),
+      fontWeight: 400,
+      lineHeight: "53.5px",
     },
     body1: {
-      fontFamily: font.style.fontFamily,
-      fontSize: "16px",
+      // normal text
+      fontFamily: appleIIPro.style.fontFamily,
+      fontSize: "24px",
       fontWeight: 400,
-      lineHeight: "24px",
-      letterSpacing: "0.5px",
+      lineHeight: "normal",
     },
     body2: {
-      fontFamily: font.style.fontFamily,
+      // leaderboard
+      fontFamily: appleIIPro.style.fontFamily,
+      fontSize: "16px",
+      fontWeight: 400,
+      lineHeight: "normal",
     },
     button: {
-      fontFamily: font.style.fontFamily,
+      fontFamily: eightBitDigi.style.fontFamily,
       fontSize: "14px",
       fontWeight: 500,
-      lineHeight: "36px",
+      lineHeight: "18.75px",
       textTransform: "uppercase" as const,
     },
     caption: {
-      fontFamily: font.style.fontFamily,
+      fontFamily: appleIIPro.style.fontFamily,
       fontSize: "12px",
       fontWeight: 400,
       lineHeight: "20px",
     },
     overline: {
-      fontFamily: font.style.fontFamily,
-      fontSize: "12px",
+      // top ranked points
+      fontFamily: ptSerif.style.fontFamily,
+      fontSize: "20px",
       fontWeight: 400,
-      lineHeight: "32px",
-      letterSpacing: "2px",
+      lineHeight: "25.88px",
+      letterSpacing: "1%",
       textTransform: "uppercase" as const,
     },
   };
