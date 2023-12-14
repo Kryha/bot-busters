4e5311e1705953a5ef3ded96a270150041bbdb6f
Privilege Mendes <privilege@kryha.io>
2023-12-14 16:16:47 +0100

    updated svgr webpack plugin

--- next.config.mjs
+++ next.config.mjs
@@ -22,22 +22,9 @@ const config = {
   },
   webpack: (webpackConfig) => {
     webpackConfig.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack'
-        },
-        {
-          loader: 'file-loader',
-          options: {
-            name: 'static/[path][name].[ext]'
-          }
-        }
-      ],
-      type: 'javascript/auto',
-      issuer: {
-        and: [/\.(ts|tsx|js|jsx|md|mdx)$/]
-      }
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
     });
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
