cd4705279c37c135190a31244304fb35ace6a6dd
Mauro Eijsenring <mauro@eijsenring.com>
2023-12-18 14:22:22 +0100

    Changed type column `matches_played` to uuid

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -21,7 +21,7 @@ export const users = bbPgTable("user", {
   score: integer("score").default(0).notNull(),
   // TODO: add zPass
   // zPass: json("zPass"),
-  matches_played: varchar("matches_played").array(),
+  matches_played: uuid("matches_played").array(),
   createdAt: timestamp("createdAt").default(sql`CURRENT_TIMESTAMP`),
 });
 
