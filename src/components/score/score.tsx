7fa976b5a92bdb4eac39533e2fa8ccecc2ab1f6d
Mauro Eijsenring <mauro@eijsenring.com>
2023-12-20 15:03:11 +0100

    Update frontend with latest refactor

--- src/components/score/score.tsx
+++ src/components/score/score.tsx
@@ -4,12 +4,13 @@ import { Divider, Stack, Typography } from "@mui/material";
 import { styles } from "./styles.js";
 import { text } from "./text.js";
 import { MATCH_ACHIEVEMENTS } from "~/server/service/achievements.js";
-import { type PlayerAchievementResult } from "~/types/match.js";
+import { type AchievementId } from "~/types/index.js";
+import { POINTS_ACHIEVEMENTS } from "~/constants/main.js";
 
 interface Props {
   correctGuesses: number;
   gainedScore: number;
-  achievements: PlayerAchievementResult[];
+  achievements: AchievementId[];
 }
 
 export const Score: FC<Props> = ({
@@ -25,17 +26,17 @@ export const Score: FC<Props> = ({
         <Stack sx={{ flexDirection: "colum" }}>
           <Stack sx={{ flexDirection: "row" }}>
             {achievements.map((achievementResult) => {
-              const { name } = MATCH_ACHIEVEMENTS[achievementResult.id] ?? {
+              const { name } = MATCH_ACHIEVEMENTS[achievementResult] ?? {
                 name: "Achievement",
               };
 
               return (
                 <>
-                  <Typography key={achievementResult.id} variant="body1">
+                  <Typography key={achievementResult} variant="body1">
                     {name}
                   </Typography>
                   <Typography variant="body1">
-                    {text.points(achievementResult.points)}
+                    {text.points(POINTS_ACHIEVEMENTS[achievementResult])}
                   </Typography>
                 </>
               );
