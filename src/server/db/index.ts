64c4fbe3b81a5dc5b1bdcbc69941e3f9ea47e4eb
Marius Sili <sili.marius.me@gmail.com>
2024-01-04 12:22:32 +0100

    feat: calculate ranks table (#155)
    ## Description
    
    This PR introduces the calculation of the ranks in a database table.
    
    ## Changes Made
    
    - SQL command to read user scores, calculate their position on the
    leaderboard and store it in the database
    - Function that wraps around the SQL command
    - Triggering score calculation on match end, score merge and user
    verification

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,10 @@
-import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { type ExtractTablesWithRelations } from "drizzle-orm";
+import {
+  type PostgresJsQueryResultHKT,
+  drizzle,
+} from "drizzle-orm/postgres-js";
+import { type PgTransaction } from "drizzle-orm/pg-core/session.js";
 
 import { env } from "~/env.mjs";
 
@@ -10,6 +15,12 @@ const queryClient = postgres(env.DATABASE_URL);
 export const db = drizzle(queryClient, { schema });
 
 export const dbSchema = schema;
+
+export type BBPgTransaction = PgTransaction<
+  PostgresJsQueryResultHKT,
+  typeof schema,
+  ExtractTablesWithRelations<typeof schema>
+>;
 
 export const closeDbConnection = async () => {
   await queryClient.end();
--- src/server/service/index.ts
+++ src/server/service/index.ts
@@ -1,2 +1,4 @@
 export * from "./agent.js";
 export * from "./match.js";
+export * from "./achievements.js";
+export * from "./leaderboard.js";
