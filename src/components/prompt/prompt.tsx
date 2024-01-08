924407a06fb9888f548cfc7b9aca2226dbbb2db2
Mauro Eijsenring <mauro@eijsenring.com>
2024-01-08 12:58:19 +0100

    Rename component prop

--- src/components/prompt/prompt.tsx
+++ src/components/prompt/prompt.tsx
@@ -5,17 +5,17 @@ import { styles } from "./styles.js";
 
 interface Props {
   name: string;
-  prompt: string;
+  message: string;
 }
 
-export const Prompt: FC<Props> = ({ name, prompt }) => {
+export const Prompt: FC<Props> = ({ name, message }) => {
   return (
     <Stack sx={styles.wrapper}>
       <Typography variant="body1" color="blueGrey.main">
         {name}
       </Typography>
       <Typography variant="h4" color="blueGrey.main">
-        {prompt}
+        {message}
       </Typography>
     </Stack>
   );
