31bbe96918d9f2823c4ed381a04d85f069426b39
Mauro Eijsenring <mauro@eijsenring.com>
2024-01-09 14:34:17 +0100

    Update username set page to connect to the wallet

--- src/pages/username-select/index.tsx
+++ src/pages/username-select/index.tsx
@@ -29,15 +29,18 @@ const styles = {
 const UsernameSelect: FC = () => {
   const router = useRouter();
   const { data: session } = useSession();
-  const { address, getSignature } = useBBWallet();
+  const { connect, isConnected, address, getSignature } = useBBWallet();
 
   const verify = api.user.verify.useMutation();
-
   useEffect(() => {
     if (isVerifiedSession(session)) {
       void router.push(pages.home);
     }
-  }, [router, session]);
+
+    if (!isConnected) {
+      void connect();
+    }
+  }, [connect, isConnected, router, session]);
 
   const handleSetUsername = async (username: string) => {
     try {
