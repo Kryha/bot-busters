6c61b040aab1d02bd1d302a46fcacf531d1f8fbe
Mauro Eijsenring <mauro@eijsenring.com>
2024-01-10 10:58:42 +0100

    Redirect to the username select page when user is signed in and does not have a username

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/router.js";
 import { default as spinner } from "~/assets/images/spinner.gif";
 import { useBBWallet } from "~/service/bb-wallet.js";
 import { text } from "~/assets/text/index.js";
-import { isVerifiedSession } from "~/utils/session.js";
+import { isUnverifiedSession, isVerifiedSession } from "~/utils/session.js";
 import { pages } from "~/router.js";
 import Page from "~/components/page/page.jsx";
 import { breakpoints } from "~/styles/theme.js";
@@ -45,7 +45,7 @@ const Login: FC = () => {
   useEffect(() => {
     const authenticate = async () => {
       try {
-        if (isVerifiedSession(session)) {
+        if (isUnverifiedSession(session)) {
           await router.push(pages.usernameSelect);
           return;
         }
