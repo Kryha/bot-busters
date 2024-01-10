129bbd39f916916db70b5d6802273cd139423afc
Mauro Eijsenring <mauro@eijsenring.com>
2024-01-10 15:08:26 +0100

    Put logic in layout page

--- src/containers/layout/layout.tsx
+++ src/containers/layout/layout.tsx
@@ -1,23 +1,40 @@
-import { type FC, useState } from "react";
+import { type FC, useState, useEffect } from "react";
 import { Container, type StackProps } from "@mui/material";
 import { useSession } from "next-auth/react";
 
-import { isVerifiedSession } from "~/utils/session.js";
+import {
+  isAnonymousSession,
+  isUnverifiedSession,
+  isVerifiedSession,
+} from "~/utils/session.js";
 import { api } from "~/utils/api.js";
 import { fakeUsername } from "~/constants/fake-data/landing.js";
 import { useBBWallet } from "~/service/bb-wallet.js";
 import { UserStats } from "~/components/user-stats/index.js";
 
 import { styles } from "./styles.js";
+import { useRouter } from "next/router.js";
+import { pages } from "~/router.js";
 
 export const Layout: FC<StackProps> = ({ children }) => {
   const { data: sessionData } = useSession();
+  const { push } = useRouter();
   const isVerifiedUser = isVerifiedSession(sessionData);
   const [open, setOpen] = useState(false);
   const { disconnect } = useBBWallet();
   const { data } = api.user.getUserById.useQuery();
   const playerPoints = data?.score ? data.score : 0;
   const isGamePlayed = true;
+
+  useEffect(() => {
+    if (isVerifiedUser) return;
+    if (!sessionData) return;
+    if (isAnonymousSession(sessionData)) return;
+
+    if (isUnverifiedSession(sessionData)) {
+      void push(pages.usernameSelect);
+    }
+  }, [sessionData]);
 
   return (
     <Container component="main" sx={styles.container}>
