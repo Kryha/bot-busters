3be97e337ed8bd0745126ccdcdb70edbd5c19468
Mauro Eijsenring <mauro@eijsenring.com>
2024-01-11 11:22:19 +0100

    fix(authentication): set username when signing in home page (#166)
    ## Description
    
    Unverified users are forced to set there username when signing in.
     
    ## Related Issues
    
    #165
    
    ## Changes Made
    
    - [BE] Update Query for setting user in the rank table 
    - [FE] Update home page to force user to set there username when not set
    but signed in

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/router.js";
 import { default as spinner } from "~/assets/images/spinner.gif";
 import { useBBWallet } from "~/service/bb-wallet.js";
 import { text } from "~/assets/text/index.js";
-import { isVerifiedSession } from "~/utils/session.js";
+import { missingUsername } from "~/utils/session.js";
 import { pages } from "~/router.js";
 import Page from "~/components/page/page.jsx";
 import { breakpoints } from "~/styles/theme.js";
@@ -45,8 +45,8 @@ const Login: FC = () => {
   useEffect(() => {
     const authenticate = async () => {
       try {
-        if (isVerifiedSession(session)) {
-          await router.push(pages.home);
+        if (missingUsername(session)) {
+          await router.push(pages.usernameSelect);
           return;
         }
 
