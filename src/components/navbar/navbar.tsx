d0d95b118ecd281f0d9312c89d9a11c867890ce0
carlos-kryha <97608823+carlos-kryha@users.noreply.github.com>
2024-01-12 14:46:30 +0100

    fix(frontend ui): refactor navbar (#175)
    fixes #163 
    
    ### Description
    Frontend navbar needed some work based on [the
    designs](https://www.figma.com/file/gyPGZoLbpWROi2CJIqbYvO/Botbusters-Design?type=design&node-id=651-17318&mode=design&t=qvsueEd2vUXsWMwA-0).
    
    Styling:
    - [x] give start and end elements of navbar a fixed (and equal) width to
    make the flex container spacing work
    - [x] lower font sizes
    - [x] lower size of icon and logo sizes
    - [x] adjust menu button states (ie. no color change on "active")
    
    Refactor:
    - [x] refactor navbar structure to use a single styles file across the
    two implementations (navbar.tsx and navbar-menu.tsx)

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -47,12 +47,17 @@ export const Navbar: FC<Props> = ({
   const handleNavigation = (path: string) => {
     void router.push(path);
   };
+
   return (
     <Stack sx={styles.container}>
       <Stack sx={styles.wrapper}>
-        <Stack sx={styles.userName}>
-          <UserIcon />
-          <Typography variant="h3">{text.general.username}</Typography>
+        <Stack sx={{ ...styles.userName, ...styles.navbarStart }}>
+          <Stack sx={styles.userIcon}>
+            <UserIcon />
+          </Stack>
+          <Typography variant="h3" sx={styles.userNameText}>
+            {text.general.username}
+          </Typography>
         </Stack>
         <Button
           variant="text"
@@ -72,7 +77,7 @@ export const Navbar: FC<Props> = ({
             {text.general.signOut}
           </Button>
         )}
-        <Stack direction={"row"} rowGap={2}>
+        <Stack direction={"row"} rowGap={2} sx={styles.navbarEnd}>
           <Button variant="text" onClick={onSoundClick}>
             {soundOn ? <SoundOnIcon /> : <SoundOffIcon />}
           </Button>
