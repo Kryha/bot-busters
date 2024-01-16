f30215684d6735b06808fb64a7d594d63821a88e
Privilege Mendes <privilege@kryha.io>
2024-01-16 20:10:24 +0100

    feat: updated character record

--- src/constants/characters/characters.ts
+++ src/constants/characters/characters.ts
@@ -3,9 +3,9 @@ import type { Character, CharacterId } from "~/types/index.js";
 export const MATCH_HOST = "host";
 
 export const CHARACTERS: Record<CharacterId, Character> = {
-  1: { id: "1", name: "ash", color: "orange", image: "" },
-  2: { id: "2", name: "eve", color: "yellow", image: "" },
-  3: { id: "3", name: "hal", color: "green", image: "" },
-  4: { id: "4", name: "dot", color: "pink", image: "" },
-  5: { id: "5", name: "roy", color: "blue", image: "" },
+  1: { id: "1", name: "ash", color: "orange" },
+  2: { id: "2", name: "eve", color: "yellow" },
+  3: { id: "3", name: "hal", color: "green" },
+  4: { id: "4", name: "dot", color: "pink" },
+  5: { id: "5", name: "roy", color: "blue" },
 };
