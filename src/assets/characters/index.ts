e8edb19c2b12f96ec59a78e895d917b211418afb
Privilege Mendes <privilege@kryha.io>
2024-01-16 19:58:16 +0100

    feat: added character avatars

--- src/assets/characters/index.ts
+++ src/assets/characters/index.ts
@@ -1,3 +1,7 @@
-import hal from "./hal.svg";
+import Hal from "./hal.svg";
+import Roy from "./roy.svg";
+import Dot from "./dot.svg";
+import Eve from "./eve.svg";
+import Ash from "./ash.svg";
 
-export { hal };
+export { Hal, Roy, Dot, Eve, Ash };
