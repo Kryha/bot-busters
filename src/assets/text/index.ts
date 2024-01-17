4288029439db0feec194c1b95420129ba017b125
Mauro Eijsenring <mauro@eijsenring.com>
2024-01-17 16:19:25 +0100

    feat(about): Created about page based on designs (#190)
    ## Description
    
    Updated the about page to match the new designes
     
    
    ## Changes Made
    
    - [FE] Added about page

--- src/assets/text/index.ts
+++ src/assets/text/index.ts
@@ -1,19 +1,22 @@
-import { auth } from "./auth.js";
-import { general } from "./general.js";
-import { navigation } from "./navigation.js";
-import { lobby } from "./lobby.js";
-import { match } from "./match.js";
-import { leaderboard } from "./leaderboard.js";
-import { homepage } from "./homepage.js";
-import { playerProfile } from "./player-profile.js";
-import { chat } from "./chat.js";
-import { matchPrompts } from "./match-prompts.js";
+import { about, wordsToLinkAbout } from "./about.js";
 import { achievements } from "./achievements.js";
+import { auth } from "./auth.js";
+import { chat } from "./chat.js";
+import { general } from "./general.js";
+import { homepage } from "./homepage.js";
+import { leaderboard } from "./leaderboard.js";
+import { lobby } from "./lobby.js";
+import { matchPrompts } from "./match-prompts.js";
+import { match } from "./match.js";
+import { navigation } from "./navigation.js";
+import { playerProfile } from "./player-profile.js";
 
 export const text = {
   auth,
+  about,
   general,
   navigation,
+  wordsToLinkAbout,
   lobby,
   match,
   leaderboard,
---
+++ src/styles/pages/about.ts
@@ -0,0 +1,24 @@
+import { type SxProps } from "@mui/material";
+
+export const styles = {
+  wrapper: {
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  } as SxProps,
+  container: {
+    width: "60vw",
+  } as SxProps,
+  header: {
+    fontWeight: "400",
+    fontStyle: "italic",
+    fontSize: "150px",
+    lineHeight: "94.5px",
+  } as SxProps,
+  body: {
+    fontWeight: "400",
+    fontSize: "24px",
+    lineHeight: "45px",
+    overflow: "wrap",
+  } as SxProps,
+};
