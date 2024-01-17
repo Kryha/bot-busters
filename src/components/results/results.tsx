c802d9f0427e74b787e6976278af219d5fe36b53
Privilege Mendes <privilege@kryha.io>
2024-01-17 08:45:59 +0100

    feat: updated homepage to test lottie files for animation + reduced ranking speed

--- src/components/results/results.tsx
+++ src/components/results/results.tsx
@@ -1,23 +1,24 @@
 import { type FC, useEffect, useState } from "react";
-import { Button, Stack, Typography } from "@mui/material";
+import { Stack } from "@mui/material";
 import { useRouter } from "next/router.js";
 import { signIn, useSession } from "next-auth/react";
 
 import { pages } from "~/router.js";
 import { useBBWallet } from "~/service/bb-wallet.js";
 import { api } from "~/utils/api.js";
-import { isAnonymousSession, missingUsername } from "~/utils/session.js";
 
 import { styles } from "./styles.js";
-import { text } from "~/assets/text/index.js";
+import { type AchievementId } from "~/types/index.js";
+import { Score } from "~/components/score/index.js";
 
 interface Props {
   gainedScore: number;
   botsBusted: number;
   totalBots: number;
+  achievements: AchievementId[];
 }
 
-export const Results: FC<Props> = ({ gainedScore, botsBusted, totalBots }) => {
+export const Results: FC<Props> = ({ gainedScore, achievements }) => {
   const { data: session } = useSession();
   const router = useRouter();
 
@@ -73,42 +74,26 @@ export const Results: FC<Props> = ({ gainedScore, botsBusted, totalBots }) => {
 
   return (
     <Stack sx={styles.wrapper}>
-      <Stack sx={styles.textContainer}>
-        <Typography variant="h2">
-          {text.chat.amountBotsBusted(botsBusted, totalBots)}
-        </Typography>
-        <Typography variant="h1">{text.chat.pointsWon(gainedScore)}</Typography>
-      </Stack>
+      <Score gainedScore={gainedScore} achievements={achievements} />
+      {/*<Stack sx={styles.buttonContainer}>*/}
+      {/*  {(isAnonymousSession(session) || missingUsername(session)) && (*/}
+      {/*    <PrimaryButton*/}
+      {/*      sx={styles.button}*/}
+      {/*      onClick={() => void handleConnect()}*/}
+      {/*      disabled={isConnecting}*/}
+      {/*    >*/}
+      {/*      {text.chat.addScore}*/}
+      {/*    </PrimaryButton>*/}
+      {/*  )}*/}
 
-      <Stack sx={styles.textWrapper}>
-        <Typography variant="body1">
-          {text.chat.addScoreToLeaderboard}
-        </Typography>
-        <Stack sx={styles.buttonContainer}>
-          {(isAnonymousSession(session) || missingUsername(session)) && (
-            <Button
-              variant="contained"
-              size="large"
-              sx={styles.button}
-              color="blueGrey"
-              onClick={() => void handleConnect()}
-              disabled={isConnecting}
-            >
-              {text.chat.addScore}
-            </Button>
-          )}
-
-          <Button
-            variant="contained"
-            size="large"
-            sx={styles.button}
-            onClick={() => void router.push(pages.home)}
-            disabled={isConnecting}
-          >
-            {text.chat.playNewGame}
-          </Button>
-        </Stack>
-      </Stack>
+      {/*  <PrimaryButton*/}
+      {/*    sx={styles.button}*/}
+      {/*    onClick={() => void router.push(pages.home)}*/}
+      {/*    disabled={isConnecting}*/}
+      {/*  >*/}
+      {/*    {text.chat.playNewGame}*/}
+      {/*  </PrimaryButton>*/}
+      {/*</Stack>*/}
     </Stack>
   );
 };
