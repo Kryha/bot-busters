37253fa6481c0538ed44079b1c295199a815f35e
Privilege Mendes <privilege@kryha.io>
2024-01-17 08:26:56 +0100

    feat: updated results stage, frontend file structure updates

--- src/components/score/styles.ts
+++ src/components/score/styles.ts
@@ -1,11 +1,32 @@
 import { type SxProps } from "@mui/material";
+import { theme } from "~/styles/theme.js";
 
 export const styles = {
   container: {
-    width: 1,
-    gap: 3,
+    flex: "1 1 auto",
+    width: "100%",
+    border: `4px solid ${theme.palette.customGrey.main}`,
+  },
+  heading: {
+    textTransform: "uppercase",
+    textAlign: "center",
+    pt: 4,
+    pb: 4,
+    pr: 2,
+    pl: 2,
+    borderBottom: `4px solid ${theme.palette.customGrey.main}`,
+  },
+  achievements: {
+    flex: "1 1 400px",
+    overflowY: "scroll",
+    pl: 2,
+    pr: 2,
   },
   score: {
+    textTransform: "uppercase",
+    flex: "0",
+    borderTop: `4px solid ${theme.palette.customGrey.main}`,
+    padding: 4,
     flexDirection: "row",
     justifyContent: "space-between",
   },
