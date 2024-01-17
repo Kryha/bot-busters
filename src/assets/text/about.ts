4a42b8feaba3b72dc0c81da6b243af8942c4e69b
Mauro Eijsenring <mauro@eijsenring.com>
2024-01-17 16:05:53 +0100

    Fix Typo

--- src/assets/text/about.ts
+++ src/assets/text/about.ts
@@ -1,13 +1,13 @@
 export const about = {
   welcome: "Welcome, Recruit",
   content: [
-    `Okay recruit. The digital landscape is teeming with rogue AI's posing as humans. Maybe they're pretending to be your grandma asking for money, or your insurance company with a "Great new offer."`,
+    `Okay recruit. The digital landscape is teaming with rogue AIs posing as humans. Maybe they're pretending to be your grandma asking for money, or your insurance company with a "Great new offer."`,
     `And where do we fit in? We're the BotBusters, making sure that thin line between real and artificial remains strong. We hunt down and bust rogue AI's to better protect the net.`,
     `Enter the BotBuster Program. It's a game that feeds questions into a chatroom. The kind of questions that humans like you and I can answer, but still trip up those nasty AI.`,
     `Stay sharp. Each conversation is a clever dance of wits and gut feelings, separating genuine human interactions from sly AI tricks. These bots will say anything to convince you they’re the real deal.`,
     `BotBusters is built by Kryha Labs on the zero-knowledge network Aleo using real privacy-preserving technology with zPass that can prove who's a real human without revealing their secrets to anyone. We can't even see it.`,
     `While playing, you can earn points based on how well you do. Every day, the top BotBusters players with the most points will receive Aleo Credits, and this bounty resets every 24 hours.`,
-    `Plus, if you're into creating your own zk solutions, you'll be happy to know Botbusters is open source. You're free to use and modify everything we've made for your own projects.`,
+    `Plus, if you're into creating your own zk solutions, you'll be happy to know BotBusters is open source. You're free to use and modify everything we've made for your own projects.`,
     `Happy hunting, recruit.`,
   ],
 };
