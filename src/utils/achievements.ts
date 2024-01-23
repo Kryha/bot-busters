8ae16a41a1a5a74800df7f0d9f17b4588ed3b891
Mauro Eijsenring <mauro@eijsenring.com>
2024-01-23 17:02:19 +0100

    Refactor function

--- src/utils/achievements.ts
+++ src/utils/achievements.ts
@@ -4,32 +4,21 @@ export const alreadyReceivedAchievement = (
   playerId: string,
   playerMatchHistory: MatchRoom[],
   achievementId: AchievementId,
+  days?: number,
 ): boolean => {
-  // Check if the achievement is in the player's achievements at any time
-  return playerMatchHistory.some((match) =>
-    match.players.some(
-      (player) =>
-        player.userId === playerId &&
-        player.achievements.includes(achievementId),
-    ),
-  );
-};
+  let playerHistory = playerMatchHistory;
 
-export const alreadyReceivedAchievementToday = (
-  playerId: string,
-  playerMatchHistory: MatchRoom[],
-  achievementId: AchievementId,
-): boolean => {
-  // Get the timestamp for 24 hours ago
-  const twentyFourHoursAgo = Date.now() - 24 * 60 * 60 * 1000;
+  if (days) {
+    // Get the timestamp for 24 hours ago
+    const twentyFourHoursAgo = Date.now() - days * 24 * 60 * 60 * 1000;
 
-  // Filter playerHistory to only include matches from the past 24 hours
-  const matchesPastDayHours = playerMatchHistory.filter(
-    (match) => match.createdAt > twentyFourHoursAgo,
-  );
-
+    // Filter playerHistory to only include matches from the past 24 hours
+    playerHistory = playerMatchHistory.filter(
+      (match) => match.createdAt > twentyFourHoursAgo,
+    );
+  }
   // Check if the achievement is in the player's achievements in the past 24 hours
-  return matchesPastDayHours.some((match) =>
+  return playerHistory.some((match) =>
     match.players.some(
       (player) =>
         player.userId === playerId &&
