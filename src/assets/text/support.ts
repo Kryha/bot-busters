43ef0c1bf9b1e7c75c3ff4028d1512c2e699e7bb
CARLOS TRIGO <carlos@kryha.io>
2024-01-24 14:03:31 +0100

    fix quotation marks on text const

--- src/assets/text/support.ts
+++ src/assets/text/support.ts
@@ -1,16 +1,16 @@
 export const support = {
   main: [
-    `Found a bug? Can't connect your wallet? Need support?`,
-    `Use the form below the BotBusters team will respond as soon as possible.`,
+    "Found a bug? Can't connect your wallet? Need support?",
+    "Use the form below the BotBusters team will respond as soon as possible.",
   ],
   placeholder: {
-    topic: `Support Topic`,
-    email: `username@email.com`,
-    issue: `Please describe the issue in as much detail as possible.`,
+    topic: "Support Topic",
+    email: "username@email.com",
+    issue: "Please describe the issue in as much detail as possible.",
   },
   input: {
-    topic: `Select Support Topic`,
-    email: `Enter Email`,
-    issue: `Describe Issue`,
+    topic: "Select Support Topic",
+    email: "Enter Email",
+    issue: "Describe Issue",
   },
 };
