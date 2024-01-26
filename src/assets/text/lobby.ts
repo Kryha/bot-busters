ecb1b9d2e70cc22a7165c4ce20c69e6bfbbc8beb
Privilege Mendes <20317699+privilegemendes@users.noreply.github.com>
2024-01-26 13:05:49 +0100

    Feat: MatchMaking Screen (#220)
    ## Description
    This PR introduces the match making screen for users to wait until they
    are assigned a room to start a game match
    
    ## Changes
    - added an event emitter for the server to notify the client if players
    join or leave the queue
    - progress bar to provide a visual indication of available players in
    the queue
    - added a character indicator to provide players a visual indication of
    which characters have been assigned and how many slots are needed to
    start a game

--- src/assets/text/lobby.ts
+++ src/assets/text/lobby.ts
@@ -1,3 +1,12 @@
 export const lobby = {
   waiting: "Waiting for others to join",
+  hostParagraph1:
+    "Hey BotBuster! Get ready for lots of different bot & human communication styles. " +
+    "This hopefully won't include offensive language or harassment, " +
+    "but hey, this is the internet we're talking about...",
+  hostParagraph2: "So have fun, be cool, and ",
+  bustSomeBotButt: "BUST SOME BOT BUTT!",
+  searching: "Searching...",
+  matchMaking: "Matchmaking",
+  progress: (val: number) => `${val} %`,
 };
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -95,4 +95,5 @@ export const breakpoints = {
   sm: 600,
   md: 960,
   lg: 1280,
+  xl: 1920,
 };
