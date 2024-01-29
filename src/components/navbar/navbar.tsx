8a8ca6fb3ed6c917cb96bd3a0ccf2f246813ca0b
carlos-kryha <97608823+carlos-kryha@users.noreply.github.com>
2024-01-29 10:49:56 +0100

    fix(menu-options): Move connect/signOut link from navbar to main-menu (#227)
    closes #224 
    
    Moves navbar connect-wallet/sign-out button to the main menu

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,6 +1,5 @@
 import { type FC, useState } from "react";
 import { Button, Stack, Typography } from "@mui/material";
-import { signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 import { text } from "~/assets/text/index.js";
 
@@ -13,7 +12,6 @@ import {
 import { MenuButton } from "~/components/main-menu/menu-button.jsx";
 import { MainMenu } from "~/components/main-menu/index.js";
 import { pages } from "~/router.js";
-import { useBBWallet } from "~/service/bb-wallet.js";
 import { styles } from "./styles.js";
 
 interface Props {
@@ -23,21 +21,9 @@ interface Props {
   setOpen: (open: boolean) => void;
 }
 
-export const Navbar: FC<Props> = ({
-  isVerifiedUser,
-  open,
-  setOpen,
-  username,
-}) => {
+export const Navbar: FC<Props> = ({ open, setOpen, username }) => {
   const router = useRouter();
-  const { disconnect: disconnectWallet } = useBBWallet();
-
   const [soundOn, setSoundOn] = useState(true);
-
-  const logOut = async () => {
-    await signOut();
-    await disconnectWallet();
-  };
 
   const onSoundClick = () => {
     setSoundOn(!soundOn);
@@ -65,17 +51,6 @@ export const Navbar: FC<Props> = ({
         >
           <BotBustersIcon />
         </Button>
-        {/*TODO: Move this to the menu*/}
-        {isVerifiedUser && (
-          <Button
-            variant="contained"
-            color="blueGrey"
-            onClick={() => void logOut()}
-            sx={styles.button}
-          >
-            {text.general.signOut}
-          </Button>
-        )}
         <Stack direction={"row"} rowGap={2} sx={styles.navbarEnd}>
           <Button variant="text" onClick={onSoundClick}>
             {soundOn ? <SoundOnIcon /> : <SoundOffIcon />}
