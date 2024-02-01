7de0d6daa89aa86c77cf923a444d24b71d6d3097
Privilege Mendes <20317699+privilegemendes@users.noreply.github.com>
2024-02-01 14:33:34 +0100

    Feat:  Added Splash Screen Animation (#243)
    ## Description
    This PR introduces animations to the splash screens. 
    
    ## Changes
    - Added animated transition lines for each characters splash screens
    - Added Lets bust some bots transition lines
    - Added animated Play Button to the homepage

--- src/components/splash-screen/splash-screen.tsx
+++ src/components/splash-screen/splash-screen.tsx
@@ -1,16 +1,27 @@
-import { Slide, Stack } from "@mui/material";
 import { type FC, type ReactNode, useEffect, useState } from "react";
+import { type CharacterName } from "~/types/index.js";
+import { Slide, Stack } from "@mui/material";
+import { AnimationPlayer } from "~/components/animation/index.js";
+import { getTransitionLines } from "~/utils/characters.jsx";
 import { SPLASH_SCREEN_DURATION } from "~/constants/index.js";
+import { theme } from "~/styles/theme.js";
 import { styles } from "./styles.js";
 
 interface Props {
   children: ReactNode;
+  characterName?: CharacterName;
   backgroundColor?: string;
 }
 
 // TODO: Splash screen timing should not overlap the chat stage timer
-export const SplashScreen: FC<Props> = ({ children, backgroundColor }) => {
+export const SplashScreen: FC<Props> = ({
+  children,
+  characterName,
+  backgroundColor = theme.palette.purple.dark,
+}) => {
   const [showSplashScreen, setShowSplashScreen] = useState(false);
+
+  const transitionLines = getTransitionLines(characterName);
 
   useEffect(() => {
     setShowSplashScreen(true);
@@ -23,7 +34,17 @@ export const SplashScreen: FC<Props> = ({ children, backgroundColor }) => {
 
   return (
     <Slide direction="left" in={showSplashScreen} mountOnEnter unmountOnExit>
-      <Stack sx={styles.container(backgroundColor)}>{children}</Stack>
+      <Stack sx={styles.container}>
+        <Stack sx={styles.transitionLines(backgroundColor)}>
+          <AnimationPlayer
+            rendererSettings={{ preserveAspectRatio: "xMidYMid slice" }}
+            animationData={transitionLines}
+            play
+            loop
+          />
+          {children}
+        </Stack>
+      </Stack>
     </Slide>
   );
 };
