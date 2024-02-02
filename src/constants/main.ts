df07187454b21e3a6f0862ad199c7874fea80bd8
Mauro Eijsenring <mauro@eijsenring.com>
2024-02-02 14:11:06 +0100

    FOR DEVELOPMENT: TEMPORARY CHANGE TO TIME PER MATCH

--- src/constants/main.ts
+++ src/constants/main.ts
@@ -5,10 +5,10 @@ export const PLACEHOLDER_TEXT = "botBusters";
 export const AUTH_SIGN_MESSAGE = "Sign in to Bot Busters";
 export const APP_NAME = "Bot Busters";
 
-export const CHAT_TIME_MS = 180000; // 3 minutes
+export const CHAT_TIME_MS = 9000; // 3 minutes
 export const MATCH_TIME_MS = 600000; // 10 minutes
 export const ALERT_TIME_MS = 30000;
-export const VOTING_TIME_MS = 60000;
+export const VOTING_TIME_MS = 9000;
 
 export const SPLASH_SCREEN_DURATION = 3000; // 3 seconds
 
