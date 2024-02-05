bdf9ffd56e37eb302de1edff2eefbc059747c9ca
Mauro Eijsenring <mauro@eijsenring.com>
2024-02-05 16:29:58 +0100

    reset chat time

--- src/constants/main.ts
+++ src/constants/main.ts
@@ -5,7 +5,7 @@ export const PLACEHOLDER_TEXT = "botBusters";
 export const AUTH_SIGN_MESSAGE = "Sign in to Bot Busters";
 export const APP_NAME = "Bot Busters";
 
-export const CHAT_TIME_MS = 9000; // 3 minutes
+export const CHAT_TIME_MS = 180000; // 3 minutes
 export const MATCH_TIME_MS = 600000; // 10 minutes
 export const ALERT_TIME_MS = 30000;
 export const VOTING_TIME_MS = 60000;
