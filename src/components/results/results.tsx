cc38185a98bae55bd34b565729abf1f3c4938bcb
Privilege Mendes <20317699+privilegemendes@users.noreply.github.com>
2024-02-06 13:42:15 +0100

    hotfix: added change to playButton text (#263)
    ## Description:
    - Updated play again button variable

--- src/components/results/results.tsx
+++ src/components/results/results.tsx
@@ -34,21 +34,27 @@ export const Results: FC<Props> = ({ gainedScore, achievements }) => {
       <Stack sx={styles.buttonContainer}>
         {loggedUser.isLoading || (
           <>
-            {!isConnectButtonHidden && (
+            <PrimaryButton
+              sx={styles.button}
+              onClick={() => void router.push(pages.lobby)}
+            >
+              {text.chat.playAgain}
+            </PrimaryButton>
+            {!isConnectButtonHidden ? (
               <PrimaryButton
                 sx={styles.button}
                 onClick={() => void router.push(pages.login)}
               >
                 {text.chat.addScore}
               </PrimaryButton>
+            ) : (
+              <PrimaryButton
+                sx={styles.button}
+                onClick={() => void router.push(pages.leaderboard)}
+              >
+                {text.chat.viewLeaderboard}
+              </PrimaryButton>
             )}
-
-            <PrimaryButton
-              sx={styles.button}
-              onClick={() => void router.push(pages.home)}
-            >
-              {text.chat.playNewGame}
-            </PrimaryButton>
           </>
         )}
       </Stack>
