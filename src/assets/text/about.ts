9b045d79a49a6cccf2007ed0ff7d5010989422d6
CARLOS TRIGO <carlos@kryha.io>
2024-02-07 10:04:20 +0100

    remove "top 10" section of about page

--- src/assets/text/about.ts
+++ src/assets/text/about.ts
@@ -6,7 +6,6 @@ export const about = {
     `Enter the BotBuster Program. It's a game that feeds questions into a chatroom. The kind of questions that humans like you and I can answer, but still trip up those nasty AI.`,
     `Stay sharp. Each conversation is a clever dance of wits and gut feelings, separating genuine human interactions from sly AI tricks. These bots will say anything to convince you they're the real deal.`,
     `BotBusters is built by Kryha Labs on the zero-knowledge network Aleo using real privacy-preserving technology with zPass that can prove who's a real human without revealing their secrets to anyone. We can't even see it.`,
-    `While playing, you can earn points based on how well you do. Every day, the top BotBusters players with the most points will receive Aleo Credits, and this bounty resets every 24 hours.`,
     `Plus, if you're into creating your own zk solutions, you'll be happy to know BotBusters is open source. You're free to use and modify everything we've made for your own projects.`,
     `Happy hunting, recruit.`,
   ],
@@ -15,4 +14,5 @@ export const wordsToLinkAbout: Record<string, string> = {
   "Kryha Labs": "https://kryha.io",
   Aleo: "https://aleo.org",
   zPass: "https://zpass.aleo.org/",
+  Obscura: "htpps://https://obscura.build/",
 };
