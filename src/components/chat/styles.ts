9bd57be87c34af1c2cb0e8877611f5ea62e151fd
Marius Sili <sili.marius.me@gmail.com>
2024-02-07 14:37:13 +0100

    fix: chat prompt width

--- src/components/chat/styles.ts
+++ src/components/chat/styles.ts
@@ -3,15 +3,13 @@ import { theme } from "~/styles/theme.js";
 import { type MatchStage } from "~/types/index.js";
 
 export const styles = {
-  section: (isDisabled: boolean) => {
-    return {
-      border: `4px solid ${theme.palette.divider}`,
-      overflow: "hidden",
-      flexGrow: 1,
-      // don't use max-width or else text will make this component wider
-      width: !isDisabled ? "65vw" : undefined,
-    } satisfies SxProps;
-  },
+  section: {
+    border: `4px solid ${theme.palette.divider}`,
+    overflow: "hidden",
+    flexGrow: 1,
+    width: "100%",
+  } satisfies SxProps,
+
   container: (stage: MatchStage) => {
     return {
       mb: 2,
