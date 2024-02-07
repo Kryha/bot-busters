c8373a4f14e0304154f8dcf7cba17a9028612557
CARLOS TRIGO <carlos@kryha.io>
2024-02-07 11:54:13 +0100

    adjust About page content

--- src/assets/text/about.ts
+++ src/assets/text/about.ts
@@ -5,7 +5,7 @@ export const about = {
     `And where do we fit in? We're the BotBusters, making sure that thin line between real and artificial remains strong. We hunt down and bust rogue AI's to better protect the net.`,
     `Enter the BotBuster Program. It's a game that feeds questions into a chatroom. The kind of questions that humans like you and I can answer, but still trip up those nasty AI.`,
     `Stay sharp. Each conversation is a clever dance of wits and gut feelings, separating genuine human interactions from sly AI tricks. These bots will say anything to convince you they're the real deal.`,
-    `BotBusters is built by Kryha Labs on the zero-knowledge network Aleo using real privacy-preserving technology with zPass that can prove who's a real human without revealing their secrets to anyone. We can't even see it.`,
+    `BotBusters is built by Kryha Labs, using Obscura, on the on the zero-knowledge network Aleo using real privacy-preserving technology with zPass that can prove who's a real human without revealing their secrets to anyone. We can't even see it.`,
     `Plus, if you're into creating your own zk solutions, you'll be happy to know BotBusters is open source. You're free to use and modify everything we've made for your own projects.`,
     `Happy hunting, recruit.`,
   ],
