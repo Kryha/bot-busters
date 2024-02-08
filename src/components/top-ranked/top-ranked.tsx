b140a169c973da5e36db28651ccbe6e332d7de87
CARLOS TRIGO <carlos@kryha.io>
2024-02-08 17:00:22 +0100

    integrate leaderboard witn topRank banner

--- src/components/top-ranked/top-ranked.tsx
+++ src/components/top-ranked/top-ranked.tsx
@@ -6,44 +6,75 @@ import { type TopRankedPlayer } from "~/types/index.js";
 
 import { styles } from "./styles.js";
 import { theme } from "~/styles/theme.js";
-// import { api } from "~/utils/api.js";
+import { api } from "~/utils/api.js";
 
 interface Props {
   players: TopRankedPlayer[];
 }
 
-export const TopRanked: FC<Props> = ({ players }) => {
+export const TopRanked: FC<Props> = () => {
   const animationStyles = [styles.topRankedBanner1, styles.topRankedBanner2];
 
-  // TODO: integrate with db instead of fake data prop
-  // const getRankedUsers = api.user.getRankedUsers.useQuery(
-  //   {
-  //     limit: 10,
-  //   },
-  // );
-
-  return (
-    <Stack sx={styles.container}>
-      <Typography variant="subtitle1" sx={styles.title}>
-        {text.homepage.topRankedTitle}
-      </Typography>
-      {animationStyles.map((sx, index) => (
-        <Stack sx={{ ...styles.textContainer, ...sx }} key={index}>
-          {players.map(({ username, score }, index) => (
-            <Typography
-              variant="subtitle2"
-              sx={styles.text}
-              color={theme.palette.common.black}
-              key={index}
-            >
-              {text.homepage.topRanked(index, username, score)}
-              <Box component={"span"} sx={styles.points}>
-                {text.homepage.points}
-              </Box>
-            </Typography>
-          ))}
-        </Stack>
-      ))}
-    </Stack>
+  const getRankedUsers = api.user.getRankedUsers.useQuery(
+    {
+      limit: 20,
+    },
   );
+
+  const rankList = getRankedUsers.data?.players;
+  
+  // Hide banner if no data available
+  if(!rankList) return <></>
+
+  // Use sliding animation if more than 2 entries
+  if(rankList.length > 2){
+    return (
+      <Stack sx={styles.container}>
+        <Typography variant="subtitle1" sx={styles.title}>
+          {text.homepage.topRankedTitle}
+        </Typography>
+        {animationStyles.map((sx, index) => (
+          <Stack sx={{ ...styles.textContainer, ...sx }} key={index}>
+            {rankList.map(({ rank, username, score }, index) => (
+              <Typography
+                variant="subtitle2"
+                sx={styles.text}
+                color={theme.palette.common.black}
+                key={index}
+              >
+                {text.homepage.topRanked(rank, username!, score)}
+                <Box component={"span"} sx={styles.points}>
+                  {text.homepage.points}
+                </Box>
+              </Typography>
+            ))}
+          </Stack>
+        ))}
+      </Stack>
+    );
+  } else {
+    // Use static list if less than 3 entries
+    return (
+      <Stack sx={styles.container}>
+        <Typography variant="subtitle1" sx={styles.title}>
+          {text.homepage.topRankedTitle}
+        </Typography>
+        <Stack sx={{ ...styles.textContainer, ...styles.textContainer }} width="100vw">
+            {rankList.map(({ rank, username, score }, index) => (
+              <Typography
+                variant="subtitle2"
+                sx={styles.text}
+                color={theme.palette.common.black}
+                key={index}
+              >
+                {text.homepage.topRanked(rank, username!, score)}
+                <Box component={"span"} sx={styles.points}>
+                  {text.homepage.points}
+                </Box>
+              </Typography>
+            ))}
+            </Stack>
+      </Stack>
+    )
+  }
 };
--- src/constants/fake-data/top-ranked.ts
+++ src/constants/fake-data/top-ranked.ts
@@ -1,10 +1,10 @@
 import { type TopRankedPlayer } from "~/types/index.js";
 
 export const TOP_RANKED_PLAYERS: TopRankedPlayer[] = [
-  { username: "Wallie Weltmeister", score: 895 },
-  { username: "Abeee001", score: 810 },
-  { username: "ramiro1999", score: 750 },
-  { username: "SilverShadow23", score: 745 },
-  { username: "LunaStarlight7", score: 289 },
-  { username: "CrimsonWanderer", score: 289 },
+  { rank: 7, username: "Wallie-Weltmei", score: 895 },
+  { rank: 7, username: "Abeee001", score: 810 },
+  { rank: 7, username: "ramiro1999", score: 750 },
+  { rank: 7, username: "SilverShadow23", score: 745 },
+  { rank: 7, username: "LunaStarlight7", score: 289 },
+  { rank: 7, username: "CrimsonWander", score: 289 },
 ];
