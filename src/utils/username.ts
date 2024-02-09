701f1b52db4f7d7f223bf463ed6f94b28c428101
Mauro Eijsenring <mauro@eijsenring.com>
2024-02-09 13:12:49 +0100

    Removed unused

--- src/utils/username.ts
+++ src/utils/username.ts
@@ -20,23 +20,3 @@ export const getRandomUsername = () => {
 
   return name;
 };
-
-// TODO: implement properly with colour logic
-export const getChatUsername = () => {
-  const colorList = ["blue", "orange", "brown", "green", "pink"];
-  const color = colorList[Math.floor(Math.random() * colorList.length)];
-
-  const userNamePart2 = faker.helpers.arrayElement([
-    faker.word.adverb(5),
-    faker.animal.cat(),
-    faker.word.adjective(5),
-    faker.animal.horse(),
-  ]);
-
-  const name = faker.internet.userName({
-    firstName: color,
-    lastName: userNamePart2,
-  });
-
-  return name;
-};
