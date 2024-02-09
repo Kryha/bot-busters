bc2e1236415777340262e41089a40329aa30ffea
Mauro Eijsenring <mauro@eijsenring.com>
2024-02-09 13:20:34 +0100

    Replace button with spinner

--- src/components/authentication/login-loading.tsx
+++ src/components/authentication/login-loading.tsx
@@ -1,13 +1,13 @@
 import { Stack, Typography } from "@mui/material";
-import { useRouter } from "next/router";
+import Image from "next/image";
+import { default as spinner } from "~/assets/images/spinner.gif";
 import { text } from "~/assets/text/index.js";
-import { PrimaryButton } from "~/components/primary-button/index.js";
 import { PageLayout } from "~/containers/page-layout/index.js";
 import { processTextToLink } from "~/utils/links.jsx";
+import { styles } from "./styles.js";
 
+const SPINNER_SIZE = 34;
 export const LoginLoading = () => {
-  const router = useRouter();
-
   return (
     <PageLayout title={text.auth.connectWallet}>
       <Stack alignItems="center" gap={4}>
@@ -17,13 +17,14 @@ export const LoginLoading = () => {
           </Typography>
         </Stack>
 
-        <PrimaryButton
-          variant="contained"
-          color="blueGrey"
-          onClick={() => void router.reload()}
-        >
-          {text.playerProfile.connectWallet}
-        </PrimaryButton>
+        <Stack sx={styles.progress}>
+          <Image
+            src={spinner}
+            alt="spinner"
+            width={SPINNER_SIZE}
+            height={SPINNER_SIZE}
+          />
+        </Stack>
       </Stack>
     </PageLayout>
   );
