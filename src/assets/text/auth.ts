f8eba7477b1ec4ee9da1569900cf03658d583521
Mauro Eijsenring <mauro@eijsenring.com>
2024-02-09 11:26:49 +0100

    Refactor link constant

--- src/assets/text/auth.ts
+++ src/assets/text/auth.ts
@@ -8,4 +8,6 @@ export const auth = {
   weUseYourWallet: "We use your Leo Wallet to verify you are a human.",
   weTransferToYourWallet:
     "We also use it to transfer your won Aleo credits to.",
+  connectWallet: "Connect wallet",
+  loginDescription: "We use Leo Wallet to verify you are a human.",
 };
