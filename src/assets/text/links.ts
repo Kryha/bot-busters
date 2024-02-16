00e4f1b8b9865d95c08be346a7fabc66f212dca5
Ramiro <ramiro@kryha.io>
2024-02-16 16:15:12 +0100

    Fix Obscura link

--- src/assets/text/links.ts
+++ src/assets/text/links.ts
@@ -2,6 +2,6 @@ export const wordsToLink: Record<string, string> = {
   "Kryha Labs": "https://kryha.io",
   Aleo: "https://aleo.org",
   zPass: "https://zpass.aleo.org/",
-  Obscura: "htpps://https://obscura.build/",
+  Obscura: "https://obscura.build/",
   "Leo Wallet": "https://www.leo.app/",
 };
