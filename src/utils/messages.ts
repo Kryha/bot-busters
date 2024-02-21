13c52cd9a3df444916ec7407ef02f5fd9351b4b8
Ramiro <ramiro@kryha.io>
2024-02-21 17:44:05 +0100

    Upgrade cleanMessage to filter extra unwanted symbols

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -42,7 +42,7 @@ export function splitMessage(input: string, maxLength: number): string[] {
 export function cleanMessage(input: string): string {
   // Removes //ufffd || </s> || *some expresion* || [INST] || (words in parenthesis) || gender symbols
   return input.replace(
-    /(\\*\\ufffd|<\/s>|(\*[^*]*\*)|(\*\w{1})\s*\w*|\[INST\]|\[\/INST\]|\[\/INST|\[INST(?:\])?|\([^)]*\)|\u2642|\u2640)/g,
+    /(\ufffd|\[\/?INST\]?|<\/s>|(\*\w+(?:\s+\w+)*\*)|\*\w+\s|\u2642|\u2640)/g,
     "",
   );
 }
