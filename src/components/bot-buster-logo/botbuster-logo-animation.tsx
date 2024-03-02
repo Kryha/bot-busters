a980364d985a21a202e530bbb3aa8a71267eaa41
Privilege Mendes <privilege@kryha.io>
2024-03-02 19:04:11 +0100

    updated homepage and animations for large screens

--- src/components/bot-buster-logo/botbuster-logo-animation.tsx
+++ src/components/bot-buster-logo/botbuster-logo-animation.tsx
@@ -1,19 +1,17 @@
 import { AnimationPlayer } from "~/components/animation/index.js";
 import { BOTBUSTERS_LOGO_ANIMATION_SEGMENT } from "~/constants/index.js";
 import { BotBustersLogo } from "~/assets/animations/index.js";
-import { Box } from "@mui/material";
 import { styles } from "./styles.js";
 
 export const BotBusterLogoAnimation = () => {
   return (
-    <Box component={"div"} sx={styles.container}>
-      <AnimationPlayer
-        animationData={BotBustersLogo}
-        segments={BOTBUSTERS_LOGO_ANIMATION_SEGMENT}
-        play
-        loop
-        style={{ width: 800, height: 220 }}
-      />
-    </Box>
+    <AnimationPlayer
+      animationData={BotBustersLogo}
+      segments={BOTBUSTERS_LOGO_ANIMATION_SEGMENT}
+      rendererSettings={{ preserveAspectRatio: "xMidYMin slice" }}
+      style={styles.container}
+      play
+      loop
+    />
   );
 };
