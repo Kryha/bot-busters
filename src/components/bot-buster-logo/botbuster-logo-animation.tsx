07d72a3bace12592b7c6aaf56082894b51f41ab3
Privilege Mendes <20317699+privilegemendes@users.noreply.github.com>
2024-03-20 14:42:40 +0100

    Bug: homepage animation fix (#361)
    ## Changes
    - shortened animation keyframes and timing
    - moved "CONTINUE GAME" button to the menu block 
    - Added static background silhouette during animation loading
    - added bot image to results transition page

--- src/components/bot-buster-logo/botbuster-logo-animation.tsx
+++ src/components/bot-buster-logo/botbuster-logo-animation.tsx
@@ -1,17 +1,41 @@
-import { AnimationPlayer } from "~/components/animation/index.js";
-import { BOTBUSTERS_LOGO_ANIMATION_SEGMENT } from "~/constants/index.js";
+import dynamic from "next/dynamic";
+import { Stack, Typography } from "@mui/material";
+import { useState } from "react";
+import { type BMEnterFrameEvent } from "lottie-web";
 import { BotBustersLogo } from "~/assets/animations/index.js";
+import { BOTBUSTERS_LOGO_ANIMATION_SEGMENT } from "~/constants/main.js";
+
+import { text } from "~/assets/text/index.js";
 import { styles } from "./styles.js";
 
+const AnimationPlayer = dynamic(
+  () => import("react-lottie-player/dist/LottiePlayerLight"),
+  {
+    ssr: false,
+  },
+);
+
 export const BotBusterLogoAnimation = () => {
+  const [showText, setShowText] = useState(false);
   return (
-    <AnimationPlayer
-      animationData={BotBustersLogo}
-      segments={BOTBUSTERS_LOGO_ANIMATION_SEGMENT}
-      rendererSettings={{ preserveAspectRatio: "xMidYMin slice" }}
-      style={styles.container}
-      play
-      loop
-    />
+    <Stack component="div" sx={styles.logo}>
+      <Typography variant="body1" sx={styles.whoIsABot(showText)}>
+        {text.homepage.descriptionPart1}
+      </Typography>
+      <AnimationPlayer
+        animationData={BotBustersLogo}
+        segments={BOTBUSTERS_LOGO_ANIMATION_SEGMENT}
+        rendererSettings={{ preserveAspectRatio: "xMidYMin slice" }}
+        style={styles.container}
+        onEnterFrame={(frame: BMEnterFrameEvent) => {
+          frame.currentTime > 0 && setShowText(true);
+        }}
+        play
+        loop
+      />
+      <Typography variant="body1" sx={styles.aleoSystems(showText)}>
+        {text.homepage.aleoSystems}
+      </Typography>
+    </Stack>
   );
 };
